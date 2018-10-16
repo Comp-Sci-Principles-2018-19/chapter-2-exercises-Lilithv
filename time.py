1ff2d2485aba0e76e7150cd0b57a9abdf1d19913
Lilith <43822266+Lilithv@users.noreply.github.com>
2018-10-16 14:25:52 -0400

    alarmt

--- time.py
+++ time.py
@@ -1,4 +1,9 @@
-time=input('what time is it')
-1700=1400*5100
-x=1700
-print(x)
+time=int(input('what time is it'))
+long=int(input('how long do you want your alarm to be for?'))
+longmodulo=long%24
+alarmt=time+longmodulo
+alarmt=alarmt%24 #dont go past 24 k thnks
+print('I will set off at',str(alarmt)+', Doctor Luigi')
+#x=5100 % 1400
+#y=x+1400
+#print(y)
